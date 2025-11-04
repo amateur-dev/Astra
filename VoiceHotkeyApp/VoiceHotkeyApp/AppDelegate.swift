d45b9c3dd94e10c09f732d094a891bc72dd66523
dipesh_sukhani <me@dipeshsukhani.dev>
2025-11-04 16:41:15 +0800

    ci: add early main.swift to run headless smoke-test before AppKit; remove @main from AppDelegate

--- VoiceHotkeyApp/VoiceHotkeyApp/AppDelegate.swift
+++ VoiceHotkeyApp/VoiceHotkeyApp/AppDelegate.swift
@@ -1,7 +1,6 @@
 import Cocoa
 import os.log
 
-@main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarController: StatusBarController?
     private let logger = OSLog(subsystem: "com.voicehotkey.app", category: "AppDelegate")
